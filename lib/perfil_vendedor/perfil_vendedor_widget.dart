1eaa91091e701e031a67eafef15726cfa9dacbe8
betabetosqui2025-oss <betabetosqui2025@gmail.com>
2025-10-24 02:14:04 -0500

    no es necesario el icon

--- lib/perfil_vendedor/perfil_vendedor_widget.dart
+++ lib/perfil_vendedor/perfil_vendedor_widget.dart
@@ -2,7 +2,6 @@ import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'perfil_vendedor_model.dart';
 export 'perfil_vendedor_model.dart';
 
